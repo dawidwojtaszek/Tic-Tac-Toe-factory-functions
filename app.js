156facef9e2b478e11072394b32f81941c278f94
unknown <dawidw22@gmail.com>
2023-03-03 14:19:52 +0100

    feat: add new game logic

--- app.js
+++ app.js
@@ -13,13 +13,101 @@ function Gameboard() {
   const setXO = (x, y, player) => {
     board[x][y].setValue(player);
   };
-  //   print to console
+
+  //  Board with tokens
+  const getBoardWithTokens = () => {
+    let boardWitaTokens = board.map((row) =>
+      row.map((cell) => {
+        if (cell.getValue().token !== undefined) {
+          return cell.getValue().token;
+        } else {
+          return 0;
+        }
+      })
+    );
+    return boardWitaTokens;
+  };
+
+  const checkIsWin = () => {
+    const boardToken = getBoardWithTokens();
+
+    let winner = null;
+
+    // horizontal
+    for (let i = 0; i < 3; i++) {
+      let counter = 0;
+      let currentE = boardToken[i][0];
+
+      for (let j = 0; j < 3; j++) {
+        if (currentE === boardToken[i][j] && boardToken[i][j] != 0) {
+          counter++;
+        }
+      }
+      if (counter === 3) {
+        winner = currentE;
+      } else {
+        counter = 0;
+      }
+    }
+
+    // vertical
+    for (let i = 0; i < 3; i++) {
+      let counter = 0;
+
+      let currentE = boardToken[0][i];
+      for (let j = 0; j < 3; j++) {
+        if (currentE === boardToken[j][i] && boardToken[j][i] != 0) {
+          counter++;
+        }
+      }
+      if (counter === 3) {
+        winner = currentE;
+      } else {
+        counter = 0;
+      }
+    }
+
+    //diagonals
+    let diagonalCounterL = 0;
+    let diagonalCounterR = 0;
+    for (let i = 0; i < 3; i++) {
+      let currentE = boardToken[0][0];
+      let currentER = boardToken[0][2];
+      for (let j = 0; j < 3; j++) {
+        if (j === i) {
+          if (currentE === boardToken[j][i] && boardToken[j][i] != 0) {
+            diagonalCounterL++;
+          }
+        }
+        if (i + j === 2) {
+          if (currentER === boardToken[j][i] && boardToken[j][i] != 0) {
+            diagonalCounterR++;
+          }
+        }
+      }
+    }
+    if (diagonalCounterL === 3) {
+      winner = boardToken[0][0];
+    } else {
+      diagonalCounterL = 0;
+    }
+    if (diagonalCounterR === 3) {
+      winner = boardToken[0][2];
+    } else {
+      diagonalCounterR = 0;
+    }
+
+    return winner;
+  };
+
   const printBoard = () => {
-    let boardToPrint = board.map((row) => row.map((cell) => cell.getValue()));
+    let boardToPrint = board.map((row) =>
+      row.map((cell) => cell.getValue().token)
+    );
     console.log(boardToPrint);
   };
 
-  return { getBoard, setXO, printBoard };
+  return { getBoard, setXO, printBoard, checkIsWin, getBoardWithTokens };
 }
 
 function Cell() {
@@ -46,22 +134,34 @@ function GameControll(
       token: 2,
     },
   ];
-
+  let round = 1;
   let activePlayer = players[0];
+
+  const getActivePlayer = () => activePlayer;
+  const resetGame = () => {
+    round = 1;
+    activePlayer = players[0];
+  };
 
   const changeActivePlayer = () => {
     activePlayer === players[0]
       ? (activePlayer = players[1])
       : (activePlayer = players[0]);
   };
+  const getRound = () => round;
 
   const playRound = (x, y) => {
+    console.log("round:", getRound(), activePlayer.name);
+    console.log(board.getBoard());
     board.setXO(x - 1, y - 1, activePlayer);
     board.printBoard();
+    console.log(board.getBoardWithTokens());
     changeActivePlayer();
+    console.log(board.checkIsWin());
+    round++;
   };
 
-  return { playRound };
+  return { playRound, getRound };
 }
 
 const game = GameControll();
